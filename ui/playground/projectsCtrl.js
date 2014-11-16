75040e480b1428051804a487feaf9378344c8612
Joaquin Tita <joaquintita@gmail.com>
2014-11-16 19:52:42 +0100

    Added dummy info

--- ui/playground/projectsCtrl.js
+++ ui/playground/projectsCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp').controller('ProjectsCtrl', ['$scope', function ($scope) {
+angular.module('myApp').controller('ProjectsCtrl', ['$scope','$modal', '$log', function ($scope, $modal, $log) {
       var owners = ["Graziela Pereiras", "Jorge Aviles Monroy", "Ke Li Huang"]
       var mySprint1 = { name: "Sprint 1", id: 1 };
       var mySprint2 = { name: "Sprint 2", id: 2 };
@@ -10,21 +10,53 @@ angular.module('myApp').controller('ProjectsCtrl', ['$scope', function ($scope) 
 
       var myProject1 = { id: 1, name: "Project 1", location: "/users/1/project/1",
                          sprints: mySprints, owner: owners[0],
-                         status:"open"
+                         status:"open",
+                         date: "10/04/2014"
                        };
 
       var myProject2 = { id: 2, name: "Project 2", location: "/users/1/project/2",
-                         sprints: [], owner: owners[1], status: "closed"
+                         sprints: [], owner: owners[1], status: "closed",
+                         date: "3/04/2014"
                        };
 
       var myProject3 = { id: 3, name: "Project 3", location: "/users/1/project/3",
-                         sprints: [], owner: owners[2], status: "open"
+                         sprints: [], owner: owners[2], status: "open",
+                         date: "22/01/2014"
                        };
 
       var myProjects = [ myProject1, myProject2, myProject3 ];
 
+
+
       $scope.projects = myProjects;
       $scope.sprints = mySprints;
 
-    }
+      
+
+  }
 ]);
+
+
+
+// myApp.factory("createNewProject", function(){
+//
+//   var newProject = { id: 0, name: "", location: "", sprints: [], owner: "",
+//                      status:"", date: "" };
+//   return {
+//       setId: function(newId){ newProject.id = newId; }
+//       setName: function(newName){ newProject.newName = name; }
+//       setLocation: function(newLocation){ newProject.location = newLocation; }
+//       setSprints: function(newSprints){ newProject.sprints = newSprints; }
+//       setOwner: function(newOwner){ newProject.owner = newOwner; }
+//       setStatus: function(newStatus){ newProject.status = newStatus; }
+//       setDate: function(newDate){ newProject.date = newDate; }
+//       getId: function(){ return newProject.id; }
+//       getName: function(){ return newProject.name; }
+//       getLocation: function(){ return newProject.location; }
+//       getSprints: function(){ return newProject.sprints; }
+//       getOwner: function(){ return newProject.owner; }
+//       getStatus: function(){ return newProject.status; }
+//       getDate: function(){ return newProject.date; }
+//   }
+//
+// });
