51cb51e122af06a41f172012fc9dd01106d889c1
Joaquin Tita <joaquintita@gmail.com>
2014-11-26 18:59:18 +0100

    added delete user

--- www/js/controllers/userCtrl.js
+++ www/js/controllers/userCtrl.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('myApp')
-  .controller('UserCtrl', ['$scope', '$location', '$log', 'RESTService', function ($scope, $location, $log, RESTService) {
+  .controller('UserCtrl', ['$scope', '$location', '$log', 'RESTService', 'AuthService', function ($scope, $location, $log, RESTService, AuthService) {
 
     var baseUsersUri = '/users'
-    var userId = 1;
+    var userId = AuthService.getUserInfo();
     var getUserUri = baseUsersUri + '/' + userId;
     var updateUserUri = baseUsersUri + "/" + userId;
+    var deleteUserUri = baseUsersUri + "/" + userId;
+
     var passwordMinLenth = 6;
 
     $scope.cancelSignUp = function(){
@@ -59,6 +61,16 @@ angular.module('myApp')
       $location.path('/user');
     }
 
+    $scope.deleteUser = function(){
+      console.log("delete user" + deleteUserUri);
+      RESTService.delete(deleteUserUri, function(data){
+        //$log.debug('Success deleting user');
+        console.log('Success deleting user');
+        AuthService.logout();
+        $location.path('/home');
+      });
+
+    }
 
   }
 ]);
