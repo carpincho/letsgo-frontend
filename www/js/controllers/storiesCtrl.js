0afefbaa2d247de95d019b9f5033fb94e32eb1b1
Jorge Isaac <jorge.aviles.m@gmail.com>
2014-11-28 05:00:43 +0100

    Delete Story

--- www/js/controllers/storiesCtrl.js
+++ www/js/controllers/storiesCtrl.js
@@ -27,35 +27,45 @@ angular.module('myApp')
   getStories();
 
 
-$scope.createStory = function(story_title, story_description,story_notes, story_points) {
+  $scope.createStory = function(story_title, story_description,story_notes, story_points) {
 
-var sprintID = $routeParams.sprintID;
-var projectID = $routeParams.projectID;
-
-
-  if (sprintID != undefined && projectID != undefined ){
-    var create_story_uri = '/projects/'+ projectID + '/sprints/'+sprintID+'/stories';
+    var sprintID = $routeParams.sprintID;
+    var projectID = $routeParams.projectID;
 
 
-    var createFormData = {
-      description: story_description,
-      title: story_title,
-      notes:story_notes,
-      points: story_points,
-      sprint_id: sprintID,
+    if (sprintID != undefined && projectID != undefined ){
+      var create_story_uri = '/projects/'+ projectID + '/sprints/'+sprintID+'/stories';
+
+
+      var createFormData = {
+        description: story_description,
+        title: story_title,
+        notes:story_notes,
+        points: story_points,
+        sprint_id: sprintID,
+      }
+
+      RESTService.post(create_story_uri, createFormData, function(data){
+        $log.debug('Success creating new Story');
+        $location.path('/projects/'+$routeParams.projectID+'/sprints/'+$routeParams.sprintID+'/stories');
+      });
+
     }
-
-    RESTService.post(create_story_uri, createFormData, function(data){
-      $log.debug('Success creating new Story');
-      $location.path('/projects/'+$routeParams.projectID+'/sprints/'+$routeParams.sprintID+'/stories');
-    });
-
   }
-}
 
-$scope.cancelCreateStory = function(){
-  $location.path('/projects/'+$routeParams.projectID+'/sprints/'+$routeParams.sprintID+'/stories');
-}
+  $scope.cancelCreateStory = function(){
+    $location.path('/projects/'+$routeParams.projectID+'/sprints/'+$routeParams.sprintID+'/stories');
+  }
+
+
+
+  $scope.deleteStory = function(projectID,sprintID,StoryId) {
+    var delete_story_uri = '/projects/'+ projectID + '/sprints/'+sprintID+'/stories/'+StoryId;
+    RESTService.delete(delete_story_uri, function(data){
+      $log.debug('Success deleting story');
+      getStories();
+    });
+  }
 
 
 }
