cac376e47821396aae8fb4a1fab18243f33734eb
Joaquin Tita <joaquintita@gmail.com>
2014-11-29 15:16:43 +0100

    basic validation in change password

--- www/js/directives/directives.js
+++ www/js/directives/directives.js
@@ -33,3 +33,21 @@ app.directive('showErrors', function() {
     }
   }
 });
+
+
+app.directive('pwCheck', [function () {
+  return {
+    require: 'ngModel',
+    link: function (scope, elem, attrs, ctrl) {
+      var firstPassword = '#' + attrs.pwCheck;
+console.log(firstPassword)
+      elem.add(firstPassword).on('keyup', function () {
+        scope.$apply(function () {
+          var v = elem.val()===$(firstPassword).val();
+          console.log(elem.val());
+          ctrl.$setValidity('pwmatch', v);
+        });
+      });
+    }
+  }
+}]);
