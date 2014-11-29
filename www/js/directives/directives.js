ce119310b879f4c3490c3342fa6d4341a909ed0f
Joaquin Tita <joaquintita@gmail.com>
2014-11-29 15:17:12 +0100

    removed console.logs

--- www/js/directives/directives.js
+++ www/js/directives/directives.js
@@ -40,11 +40,9 @@ app.directive('pwCheck', [function () {
     require: 'ngModel',
     link: function (scope, elem, attrs, ctrl) {
       var firstPassword = '#' + attrs.pwCheck;
-console.log(firstPassword)
       elem.add(firstPassword).on('keyup', function () {
         scope.$apply(function () {
           var v = elem.val()===$(firstPassword).val();
-          console.log(elem.val());
           ctrl.$setValidity('pwmatch', v);
         });
       });
