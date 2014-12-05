e5d291d2bfea660adf1319738ab2ea015a579899
Joaquin Tita <joaquintita@gmail.com>
2014-12-05 19:17:53 +0100

    fixed button

--- www/js/controllers/sessionCtrl.js
+++ www/js/controllers/sessionCtrl.js
@@ -3,5 +3,8 @@
 angular.module('myApp')
   .controller('SessionCtrl', ['$scope', '$http', '$log', '$timeout','$cookieStore', '$location', 'AuthService',
     function ($scope, $http, $log, $timeout, $cookieStore, $location, AuthService) {
+      if (AuthService.isLoggedIn()){
+        $location.path("/projects")
+      }
     }
   ]);
